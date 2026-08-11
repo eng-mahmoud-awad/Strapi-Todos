b4b728a7c11c4846b5e8a8e7bb4eaeb99168aa2c
eng-mahmoud-awad <mahmoud.awad.mo1@gmail.com>
2026-08-11 04:21:19 +0300

    ggg

--- config/server.ts
+++ config/server.ts
@@ -9,6 +9,7 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server =>
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  proxy: true,
 });
 
 export default config;
